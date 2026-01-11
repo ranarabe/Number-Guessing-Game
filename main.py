a7ce147c664d98a7fa00fc9a480c7440eb5f0308
ranarabe <rana.t.rabee@gmail.com>
2026-01-11 23:41:52 +0100

    Add core implementation

---
+++ main.py
@@ -0,0 +1,91 @@
+import os
+import sys
+import datetime
+import random
+
+global generated_number , difficulty_chance
+generated_number = random.randint(1,100)
+difficulty_chance ={ '1': 10 , "2":5 ,"3":3 }
+
+
+
+def start_the_game(imp=0):
+    global generated_number
+
+    while imp == 0 :
+
+        difficulty_ID= int(input('''Please select the difficulty level by its id:
+            1. Easy (10 chances)
+            2. Medium (5 chances)
+            3. Hard (3 chances)
+            '''))
+
+        if difficulty_ID not in [1,2,3] :
+            print("The Difficulty ID is not exist use on of [1,2,3]")
+        else:
+            difficulty = int(difficulty_chance[str(difficulty_ID)])
+            imp = 1 
+
+
+    guessed= False
+
+    for  chance in range(0,difficulty) :
+        guess = input("Enter your guess:")
+        try:
+            guess = int(guess)
+        except :
+            print('''Your input is not valid!! , you lost one chance you still have {(difficulty-1)-chance} chances left ,
+            try again with number between 1-100.''')
+            continue
+        if  isinstance(guess, int) and ((int(guess) < 100) and (int(guess) > 0) ):
+            if guess == generated_number :
+                guessed=True
+                input_text = f""" Congratulations! You guessed the correct number in {chance} attempts ,
+                Do you want to play again ?? [yes/no]"""
+                again = input(input_text)
+                break
+            else :
+                print(f"Your guess is not correct , you have {difficulty-1-chance} chances left ")
+        else :
+            print('''Your input is not valid!! , you lost one chance you still have {(difficulty-1)-chance} chances left ,
+             try again with number between 1-100.''')
+
+    if guessed == False :
+        print(f"Unfortunately! You did not guess the correct number which was {generated_number} in {chance+1} attempts ")
+        input_text1 = input("Do you want to play again ?? [yes/no]")
+        
+    while input_text1 not in ["yes","no"]:
+        print("Your input key is nat valid write yes or no ")
+    again= input(input_text1)
+    
+    if again == "yes" :
+        generated_number = random.randint(1,100)
+        start_the_game()
+    elif  again == "no":
+        print("Okay bye bye !!!")
+    else :
+        print("Error input ")
+        
+
+    
+if __name__ == "__main__":
+
+    print('''*** Welcome to the Number Guessing Game! ***
+    I'm thinking of a number between 1 and 100.
+    You have many chances to guess the correct number. ''')
+    first_input = input("\t Do you want to play ?? [yes/no]")
+
+    while first_input not in ["yes","no"]:
+        print("Your input key is nat valid write yes or no ")
+        first_input = input("\t Do you want to play ?? [yes/no]")
+
+    if first_input == 'yes':
+        
+        start_the_game()
+
+    elif first_input == "no":
+        print("Okay bye bye !!!")
+    else :
+        print("Error input ")
+
+
